7f4754c758a7776273d69a41ec141d95abc4c412
akshat290999 <akshatpandey13@gmail.com>
2026-06-19 18:42:31 +0530

    UI: Add scroll to top on route change and global fade-in animations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 
@@ -9,19 +10,40 @@ import PlatformDetail from './pages/PlatformDetail'
 import IntlVsIndia from './pages/IntlVsIndia'
 import BuildPlanner from './pages/BuildPlanner'
 
+function AppContent() {
+  const location = useLocation()
+
+  // Scroll to top on every route change
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [location.pathname])
+
+  return (
+    <>
+      <Navbar />
+      {/* 
+        The key attribute forces React to remount this div on every route change,
+        which re-triggers the .page-transition CSS animation.
+      */}
+      <div key={location.pathname} className="page-transition">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/basics" element={<TuningBasics />} />
+          <Route path="/platforms" element={<Platforms />} />
+          <Route path="/tuners" element={<TunersAndCosts />} />
+          <Route path="/platform/:id" element={<PlatformDetail />} />
+          <Route path="/intl" element={<IntlVsIndia />} />
+          <Route path="/build" element={<BuildPlanner />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/basics" element={<TuningBasics />} />
-        <Route path="/platforms" element={<Platforms />} />
-        <Route path="/tuners" element={<TunersAndCosts />} />
-        <Route path="/platform/:id" element={<PlatformDetail />} />
-        <Route path="/intl" element={<IntlVsIndia />} />
-        <Route path="/build" element={<BuildPlanner />} />
-      </Routes>
+      <AppContent />
     </BrowserRouter>
   )
 }
